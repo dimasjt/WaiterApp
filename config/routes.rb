1e7d1b10bcd559fc16bb35431b4ae4c3578036fb
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-09-25 17:12:06 +0700

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   root to: "pages#home"
 
   namespace :api, default: { format: :json } do
+    post "login", to: "sessions#create", as: :login
+
     resources :users
   end
 end
