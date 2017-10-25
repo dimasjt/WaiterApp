a540e139d353e582edbf5e4ebee072a9bf7ef0fe
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-10-25 16:27:13 +0700

    setup redux apollo router

--- app/javascript/packs/App.js
+++ app/javascript/packs/App.js
@@ -1,16 +1,21 @@
 import React from "react"
 import { MuiThemeProvider, createMuiTheme } from "material-ui/styles"
+import { ApolloProvider } from "react-apollo"
 
 import "typeface-roboto"
 
 import Routes from "./components/Routes"
+import { store } from "./store"
+import { apolloClient } from "./apollo"
 
 const theme = createMuiTheme()
 
 const App = () => (
-  <MuiThemeProvider theme={theme}>
-    <Routes />
-  </MuiThemeProvider>
+  <ApolloProvider store={store} client={apolloClient}>
+    <MuiThemeProvider theme={theme}>
+      <Routes />
+    </MuiThemeProvider>
+  </ApolloProvider>
 )
 
 export default App
