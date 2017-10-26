9d10e3708fb7e1138d960f83330b2093e47d1709
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-10-26 11:11:20 +0700

    add select

--- app/javascript/packs/components/Fields.js
+++ app/javascript/packs/components/Fields.js
@@ -1,5 +1,9 @@
 import TextField from "./forms/TextField"
+import AutoComplete from "./forms/AutoComplete"
+import SelectField from "./forms/SelectField"
 
 export {
   TextField,
+  AutoComplete,
+  SelectField,
 }
--- app/javascript/packs/pages/AddProductPage.js
+++ app/javascript/packs/pages/AddProductPage.js
@@ -4,13 +4,20 @@ import { withStyles } from "material-ui/styles"
 import { Button } from "material-ui"
 import PropTypes from "prop-types"
 
-import { TextField } from "../components/Fields"
+import { TextField, SelectField } from "../components/Fields"
 
 const styles = (theme) => ({
   root: {
     padding: 12,
   },
 })
+
+const options = [
+  { id: 1, name: "Main" },
+  { id: 2, name: "Drink" },
+  { id: 3, name: "Beverage" },
+  { id: 4, name: "Snack" },
+]
 
 class AddProductPage extends Component {
   render() {
@@ -22,6 +29,7 @@ class AddProductPage extends Component {
         <TextField name="price" />
         <TextField name="sku" label="SKU" />
         <TextField name="description" multiline />
+        <SelectField name="category_id" label="Category" options={options} />
         <Button>
           Submit
         </Button>
